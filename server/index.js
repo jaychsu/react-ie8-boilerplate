f1402aa445d38a6bcba002a046175b8854c4e31b
Jaych Su <jaych.su@autodesk.com>
2016-11-30 17:16:59 +0800

    Import library: ‘pug’

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ var path = require('path')
 var express = require('express')
 var app = express()
 
+// Set template engine
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, './view/'))
+
 app.use(express.static(path.join(__dirname, '../client/')))
 require('./route')(app)
 require('./public')(app)
--- server/route.js
+++ server/route.js
@@ -1,5 +1,9 @@
 'use strict'
 
 module.exports = function (app) {
-
+  app.get('/', function (req, res) {
+    res.render('index', {
+      title: 'Autodesk Accounts'
+    })
+  })
 }
