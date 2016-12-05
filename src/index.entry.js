6d3ac1d030e0fd3b594eb5098e7bfe9eb958fbe0
Jaych Su <jaych.su@autodesk.com>
2016-12-05 13:57:29 +0800

    Enables `redux` in proj

--- src/index.entry.js
+++ src/index.entry.js
@@ -3,12 +3,23 @@ import 'normalize.css'
 import 'util/variable.less'
 import 'util/global.less'
 
-// Init React
+// `react`
 import React from 'react'
 import { render } from 'react-dom'
+
+// `react-router`
 import AppRouter from './route'
 
+// `redux`
+import { Provider } from 'react-redux'
+import configStore from './store'
+const store = configStore()
+
 render(
-  <AppRouter />,
+  (
+    <Provider store={ store }>
+      <AppRouter />
+    </Provider>
+  ),
   document.getElementById('app')
 )
---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import {
+  createStore,
+  applyMiddleware
+} from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import createLogger from 'redux-logger'
+import reducers from './reducer'
+
+export default function configStore(initState) {
+  const store = createStore(
+    reducers,
+    initState,
+    applyMiddleware(thunkMiddleware, createLogger())
+  )
+
+  return store
+}
