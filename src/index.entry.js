4c5fb137ec8f7abb4b26a9bbe79a1500e2dcb84e
Jaych Su <jaych.su@autodesk.com>
2016-12-05 15:37:57 +0800

    Enables `redux` with `react-router`

--- src/index.entry.js
+++ src/index.entry.js
@@ -6,20 +6,11 @@ import 'util/global.less'
 // `react`
 import React from 'react'
 import { render } from 'react-dom'
-
-// `react-router`
 import AppRouter from './route'
-
-// `redux`
-import { Provider } from 'react-redux'
-import configStore from './store'
-const store = configStore()
 
 render(
   (
-    <Provider store={ store }>
-      <AppRouter />
-    </Provider>
+    <AppRouter />
   ),
   document.getElementById('app')
 )
