448f4044bca9c96b606b7d32d86eb8ea80e6754a
Jaych Su <jaych.su@autodesk.com>
2016-12-12 12:54:53 +0800

    Add demo to show ABTest

--- src/container/home.js
+++ src/container/home.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react'
+import {
+  Experiment,
+  Variant,
+  emitter,
+} from 'react-ab-test'
+
 import Header from 'component/header'
+import Section from 'component/section'
+
+emitter.defineVariants('ABTest', ['A', 'B', 'C'], [10, 40, 40])
 
 export default class Home extends Component {
   render() {
     return (
-      <Header title="Homepage" />
+      <div>
+        <Header title="Homepage" />
+        <Experiment name="ABTest">
+          <Variant name="A">
+            <Section
+              title="Section A"
+              content={ <div /> }
+            />
+          </Variant>
+          <Variant name="B">
+            <Section
+              title="Section B"
+              content={ <div /> }
+            />
+          </Variant>
+          <Variant name="C">
+            <Section
+              title="Section C"
+              content={ <div /> }
+            />
+          </Variant>
+        </Experiment>
+      </div>
     )
   }
 }
